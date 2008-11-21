f92452003e4609120c99108138e5140456243ffa
stefan kersten <sk@k-hornz.de>
2008-11-21 12:24:44 +0100

    Add documentation for `deriveAccessors'
    darcs-hash:20081121112444-dde64-8cc6c75935dcb67b07e9515e3d23e85479b9bb43.gz

--- Sound/SC3/Server/Process/Accessor.hs
+++ Sound/SC3/Server/Process/Accessor.hs
@@ -5,5 +5,6 @@ module Sound.SC3.Server.Process.Accessor (
 import qualified Data.Accessor.Template as Accessor
 import Language.Haskell.TH              (Dec, Name, Q)
 
+-- | Derive accessors with a leading @_@.
 deriveAccessors :: Name -> Q [Dec]
 deriveAccessors = flip Accessor.nameDeriveAccessors (Just . ("_"++))
