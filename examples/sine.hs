c6e6e1235359c803b7c97ad25b6530dacd306667
Stefan Kersten <sk@xdv.org>
2009-11-27 17:12:30 +0100

    Update the sine example
    Ignore-this: 3d470293b62374a39ea3d993975594b9
    
    darcs-hash:20091127161230-15200-1e2b60f0c305044ff8706c4762f0d778cd1f1797.gz

--- examples/sine.hs
+++ examples/sine.hs
@@ -1,13 +1,20 @@
-import Sound.OpenSoundControl   (UDP)
+import Sound.OpenSoundControl (OSC(..), Time(..), UDP, pauseThread)
 import Sound.SC3
 import Sound.SC3.Server.Process
 import Data.Accessor
 
+sine :: UGen
+sine = out 0 (mce2 (s 390) (s 400))
+    where s f = sinOsc AR f 0.1 * 0.3
+
 scmain :: UDP -> IO ()
-scmain _ = return ()
+scmain fd = do
+    reset fd
+    play fd sine
+    pauseThread 4
+    send fd quit
 
 main :: IO ()   
 main = do
-    putStrLn "fuck"
-    withSynth defaultServerOptions defaultRTOptionsUDP ((onBoot^=scmain) defaultEventHandler)
+    withSynth defaultServerOptions defaultRTOptionsUDP (defaultEventHandler { onBoot = scmain })
     return ()
